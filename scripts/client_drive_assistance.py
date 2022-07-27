67a541c3b853ee323ac9227c1d037783250da54a
raja-sahab <96690967+raja-sahab@users.noreply.github.com>
2022-07-27 18:09:10 +0000

    Add files via upload

--- scripts/client_drive_assistance.py
+++ scripts/client_drive_assistance.py
@@ -1,65 +1,65 @@
 #! /usr/bin/env python3
 
-# Run with: roslaunch final_assignment assignment.launch
+''' Run with: roslaunch final_assignment assignment.launch '''
 
-### LIBRARIES ###
+### LIBRARIES '''
 import rospy
 import curses
 
-### SERVICE ###
+''' SERVICE '''
 from final_assignment.srv import Command
 
-### CODE ###
+'''CODE '''
 def drive_assistance(ui):
-    # Function to drive the robot througha controller that avoids hurting obstacles.
+    ''' Function to drive the robot througha controller that avoids hurting obstacles. '''
 
-    # Creates a client to the command service.
+    ''' Creates a client to the command service. '''
     client = rospy.ServiceProxy("/command", Command)
-    char = 'n'          # Sets the variable char to a default value.
+    char = 'n'          ''' Sets the variable char to a default value.'''
 
-    curses.noecho()     # From now on, the terminal does not print the pressed key.
-    curses.cbreak()     # From now on, the input does not wait for the enter key.
+    curses.noecho()     ''' From now on, the terminal does not print the pressed key. '''
+    curses.cbreak()     ''' From now on, the input does not wait for the enter key. '''
 
-    ui.set_wasd()       # Shows the commands for the free drive modality.
+    ui.set_wasd()       ''' Shows the commands for the free drive modality. '''
     ui.clear_info()
 
-    # Waits until the command service is active.
+    ''' Waits until the command service is active. '''
     rospy.wait_for_service("/command")
 
-    # Send the request of enabling the driver_assistance node.
+    ''' Send the request of enabling the driver_assistance node. '''
     client(ord('0'))
 
     while True:
 
-        char = ui.win_input.getch()   # Gets the user command.
+        char = ui.win_input.getch()   # Gets the user command. '''
         ui.clear_input()
 
-        if char == ord('b'):   # Exits from the while loop.
+        if char == ord('b'):   # Exits from the while loop. '''
             ui.clear_modes()  
             break
 
-        # The character is one of the availables.
+        ''' The character is one of the availables. '''
         elif char == ord('w') or char == ord('s') or char == ord('d') or char == ord('a') or char == ord('x') or char == ord('z'):
-            # Send a request to the server with the user input character.
+            # Send a request to the server with the user input character. '''
             resp = client(char)
 
-            # Prints on the info window the linear speed.
+            ''' Prints on the info window the linear speed. '''
             msg_linear = "Linear velocity: %.1f  " % resp.linear
             ui.win_info.addstr(2, 1, msg_linear)
 
-            # Prints on the info window the angular speed.
+            ''' Prints on the info window the angular speed. '''
             msg_angular = "Angular velocity: %.1f  " % resp.angular
             ui.win_info.addstr(3, 1, msg_angular)
             ui.win_info.refresh()
 
-    # Stops the robot, sending a zero velocity.
+    ''' Stops the robot, sending a zero velocity. '''
     client(ord('x'))
     client(ord('z'))
     client(ord('1'))
 
-    # Restores the default terminal paramethers.
+    ''' Restores the default terminal paramethers. '''
     curses.echo()
     curses.nocbreak()
 
-    # Prints the old avaliable commands.
+    ''' Prints the old avaliable commands. '''
     ui.clear_modes()
--- scripts/free_drive.py
+++ scripts/free_drive.py
@@ -1,82 +1,82 @@
 #! /usr/bin/env python3
 
-# Run with: roslaunch final_assignment assignment.launch
+''' Run with: roslaunch final_assignment assignment.launch '''
 
-### LIBRARIES ###
+''' LIBRARIES '''
 import rospy
 import curses
 
-### MESSAGE ###
+''' MESSAGE '''
 from geometry_msgs.msg import Twist
 
-### CODE ###
+''' CODE '''
 def free_drive(ui):
-    # Function to drive directly the robot, controlling the speed publishing on the cmd_vel topic.
+     ''' Function to drive directly the robot, controlling the speed publishing on the cmd_vel topic. ''''
 
-    # Creates a publisher to the cmd_vel topic.
+     ''' Creates a publisher to the cmd_vel topic. '''
     pub = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 
-    char = 'n'          # Sets the variable char to a default value.
-    straight = 0        # Sets the linear velocity to zero.
-    turn = 0            # Sets the angular velocity to zero.
-    my_vel = Twist();   # Creates a Twist message.
+    char = 'n'          ''' Sets the variable char to a default value. '''
+    straight = 0        ''' Sets the linear velocity to zero. '''
+    turn = 0            ''' Sets the angular velocity to zero. '''
+    my_vel = Twist();   ''' Creates a Twist message. '''
 
-    curses.noecho()     # From now on, the terminal does not print the pressed key.
-    curses.cbreak()     # From now on, the input does not wait for the enter key.
+    curses.noecho()     ''' From now on, the terminal does not print the pressed key. '''
+    curses.cbreak()     ''' From now on, the input does not wait for the enter key. '''
 
-    ui.set_wasd()       # Shows the commands for the free drive modality.
+    ui.set_wasd()       ''' Shows the commands for the free drive modality. '''
     ui.clear_info()
 
     while True:
 
-        char = ui.win_input.getch()   # Gets the user command.
+        char = ui.win_input.getch()   ''' Gets the user command. '''
         ui.clear_input()
 
-        # Understands the user command.
-        if char == ord('b'):     # Exits from the while loop.
+        ''' Understands the user command. '''
+        if char == ord('b'):     ''' Exits from the while loop. '''
             ui.clear_modes()
             break
 
-        elif char == ord('w'):   # Increases linear speed.
+        elif char == ord('w'):   ''' Increases linear speed. '''
             straight += 0.1
 
-        elif char == ord('s'):   # Decreases linear speed.
+        elif char == ord('s'):   ''' Decreases linear speed. '''
             straight += -0.1
 
-        elif char == ord('d'):   # Decreases ang. speed (right)
+        elif char == ord('d'):   ''' Decreases ang. speed (right) '''
             turn += -0.1
 
-        elif char == ord('a'):   # Increases ang. speed (left)
+        elif char == ord('a'):   ''' Increases ang. speed (left) '''
             turn += 0.1
 
-        elif char == ord('x'):   # Stops linear speed
+        elif char == ord('x'):   ''' Stops linear speed '''
             straight = 0
 
-        elif char == ord('z'):   # Stops angular speed
+        elif char == ord('z'):   ''' Stops angular speed '''
             turn = 0
 
-        # Prints on the info window the linear speed.
+        ''' Prints on the info window the linear speed. '''
         msg_linear = "Linear velocity: %.1f  " % straight
         ui.win_info.addstr(2, 1, msg_linear)
 
-        # Prints on the info window the angular speed.
+        ''' Prints on the info window the angular speed. '''
         msg_angular = "Angular velocity: %.1f  " % turn
         ui.win_info.addstr(3, 1, msg_angular)
         ui.win_info.refresh()
 
-        # Publishes the speed on the topic my_vel.
+        ''' Publishes the speed on the topic my_vel. '''
         my_vel.linear.x = straight;
         my_vel.angular.z = turn;
         pub.publish(my_vel);
 
-    # Stops the robot, sending a zero velocity.
+    ''' Stops the robot, sending a zero velocity. '''
     my_vel.linear.x = 0;
     my_vel.angular.z = 0;
     pub.publish(my_vel);
 
-    # Restores the default terminal paramethers.
+    ''' Restores the default terminal paramethers. '''
     curses.echo()
     curses.nocbreak()
 
-    # Prints the old avaliable commands.
+    ''' Prints the old avaliable commands. '''
     ui.clear_modes()
